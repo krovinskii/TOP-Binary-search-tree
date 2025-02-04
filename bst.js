dffb5a613cad267fc681efb5e9df44d5277c5d5b
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2025-02-03 23:04:50 -0600

    tree displayed

--- bst.js
+++ bst.js
@@ -1,20 +1,14 @@
 class Node {
-  //Individual items
-  constructor(data, left, right) {
+  constructor(data, left = null, right = null) {
     this.data = data;
     this.left = left;
     this.right = right;
   }
 }
-class Tree {
-  //Node pathway
-  constructor(arr) {
-    this.root = buildTree(arr);
-  }
-}
+
 const buildTree = (arr) => {
-  //Takes arr and turns it into balanced binary tree full of Node objects
-  //Returns the level-0 root node
+  // Takes arr and turns it into balanced binary tree full of Node objects
+  // Returns the level-0 root node
   if (arr.length === 1) {
     return new Node(arr[0]);
   } else if (arr.length === 0) {
@@ -27,9 +21,26 @@ const buildTree = (arr) => {
     const root = arr[rootIndex];
     const left = arr.slice(0, rootIndex);
     const right = arr.slice(rootIndex + 1);
-
     const leftNode = buildTree(left);
     const rightNode = buildTree(right);
     return new Node(root, leftNode, rightNode);
   }
 };
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  //display tree
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const rootNode = buildTree(arr);
+prettyPrint(rootNode);
