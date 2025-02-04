e409777477199ef7868088d208a4a7aef73df32f
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2025-02-03 22:56:59 -0600

    buildTree func done

--- bst.js
+++ bst.js
@@ -1,9 +1,9 @@
 class Node {
   //Individual items
-  constructor(data) {
+  constructor(data, left, right) {
     this.data = data;
-    this.left = null;
-    this.right = null;
+    this.left = left;
+    this.right = right;
   }
 }
 class Tree {
@@ -15,4 +15,21 @@ class Tree {
 const buildTree = (arr) => {
   //Takes arr and turns it into balanced binary tree full of Node objects
   //Returns the level-0 root node
+  if (arr.length === 1) {
+    return new Node(arr[0]);
+  } else if (arr.length === 0) {
+    return null;
+  } else {
+    arr.sort(function (a, b) {
+      return a - b; // sort ascending
+    });
+    const rootIndex = Math.floor(arr.length / 2);
+    const root = arr[rootIndex];
+    const left = arr.slice(0, rootIndex);
+    const right = arr.slice(rootIndex + 1);
+
+    const leftNode = buildTree(left);
+    const rightNode = buildTree(right);
+    return new Node(root, leftNode, rightNode);
+  }
 };
